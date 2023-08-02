27b9a83d75f2eea065fddd7829cba0c9bf232632
Christian Daniel Aleman Pericon <christian.c.aleman@gmail.com>
2023-08-02 18:48:38 +0400

    Update SBI_nrm.m

--- SBI_nrm.m
+++ SBI_nrm.m
@@ -18,7 +18,7 @@ Technical Requirements:
 toolboxes
 
 DESCRIPTION
-SBI_norm calls functions:
+SBI_nrm calls functions:
     -smooth_ts: Smooths series, using a chosen smoother
     -norm_func: Performs normalization C to T and T to C, computes policy estimates and
         figures, see, figure index at the end.
